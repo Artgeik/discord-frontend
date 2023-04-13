64c0a81828f0a81cf0db513a390ef5a7e261f775
Rocketeer <68785035+Artgeik@users.noreply.github.com>
2023-04-13 21:21:22 +0530

    added not port

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
-    baseURL:'https://disc-backend.onrender.com:443',
+    baseURL:'https://disc-backend.onrender.com',
     timeout:1000
 });
 
