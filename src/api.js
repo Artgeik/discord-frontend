836b651cafba85ed05015dafcae784aefd19edb7
Rocketeer <68785035+Artgeik@users.noreply.github.com>
2023-04-13 17:56:17 +0530

    changed teh link anddress again

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
-    baseURL:'https://disc-backend.onrender.com/:3002',
+    baseURL:'https://disc-backend.onrender.com:10000',
     timeout:1000
 });
 
